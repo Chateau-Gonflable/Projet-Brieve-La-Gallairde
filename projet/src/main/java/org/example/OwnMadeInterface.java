fd867d6fb8927fead9d6e4cfe740d7f00b14ab07
Kaputchino <andreasmulard@gmail.com>
2023-02-13 11:08:50 +0100

    modif

--- projet/src/main/java/org/example/OwnMadeInterface.java
+++ projet/src/main/java/org/example/OwnMadeInterface.java
@@ -9,7 +9,7 @@ public class OwnMadeInterface extends JFrame {
     Graphics g;
     OwnMadeInterface(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(300,300);
+        this.setSize(2500,2500);
         JButton button = new JButton("Press");
         JPanel panel = new JPanel();
         Node n1 = new Node();
@@ -17,9 +17,19 @@ public class OwnMadeInterface extends JFrame {
         Node n3 = new Node();
         Node n4 = new Node();
         Node n5 = new Node();
+        NodeGUI ng1 = new NodeGUI(n1,1);
+        NodeGUI ng2 = new NodeGUI(n1,2);
+        NodeGUI ng3 = new NodeGUI(n1,3);
+        NodeGUI ng4 = new NodeGUI(n1,4);
+        NodeGUI ng5 = new NodeGUI(n1,5);
+        panel.add(ng1);
+        panel.add(ng2);
+        panel.add(ng3);
+        panel.add(ng4);
+        panel.add(ng5);
 
 
-        this.getContentPane().add(panel); // Adds Button to content pane of frame
+        this.getContentPane().add(panel);
         this.setVisible(true);
     }
     public void drawPort(Port begin, Port end, int height){
@@ -28,7 +38,10 @@ public class OwnMadeInterface extends JFrame {
         g.drawLine(begin.y,height,end.x,begin.y);
         drawArrow(end);
     }
-    public void drawArrow(Port end){}
+    //TO DO
+    public void drawArrow(Port end){
+
+    }
     @Override
     public void paint(Graphics g) {
         this.g =  g;
