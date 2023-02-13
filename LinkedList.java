08cde6041907a17d0c44744ef0483548c4f5ad08
Alpha-Lin <alpha_lin@protonmail.com>
2023-02-13 10:14:59 +0100

    Ensure changing accumulator when it should be

--- LinkedList.java
+++ LinkedList.java
@@ -1,18 +1,18 @@
-public class LinkedList
-{
+public class LinkedList {
 	class Node {
 		Object info; // éléments, item de donnée
 		Node next; // fait reference au Node next dans la liste
-		Node( Object ob ) // constructeur
+
+		Node(Object ob) // constructeur
 		{
 			info = ob;
 			next = null;
 		}
 	}
 
-	Node head; 
-	Node current; 
-	int numberOfElements; 
+	Node head;
+	Node current;
+	int numberOfElements;
 
 	public void initList() {
 		head = new Node(null);
@@ -28,13 +28,14 @@ public class LinkedList
 	 */
 	public void createList(int n, int increment) {
 		initList();
-		for(int i = n; i >= increment; i = i - increment) {
+		for (int i = n; i >= increment; i = i - increment) {
 			this.insertHead(i);
 		}
 	}
 
 	/**
 	 * Insert a new head an shift the list.
+	 * 
 	 * @param ob
 	 */
 	public void insertHead(Object ob) {
@@ -47,69 +48,78 @@ public class LinkedList
 
 	/**
 	 * Insert an objet right after another one.
+	 * 
 	 * @param obNew
 	 * @param ob
 	 */
 	public void insertAfter(Object obNew, Object ob) {
 		current = head;
-		while(current.next != null) {
+		while (current.next != null) {
 
 			// node_a - > node_b
 			// node_a -> newNode -> node_b
 			// current is node_a in this exemple.
 			if (current.info == ob) {
-					Node newNode = new Node(obNew); // Create a new node.
-					newNode.next = current.next;  // Linking it to node_a's next: node_b
-					current.next = newNode; // node's a next becomes newNode
+				Node newNode = new Node(obNew); // Create a new node.
+				newNode.next = current.next; // Linking it to node_a's next: node_b
+				current.next = newNode; // node's a next becomes newNode
+
+				numberOfElements++;
 			}
-			
+
 			current = current.next; // aka the new node.
 		}
-		
-		numberOfElements++;
 	}
 
 	/**
 	 * Deletes the head.
+	 * 
 	 * @return
 	 */
 	public Object deleteHead() {
-		Node toDelete = head;
-		Object toDeleteInfo = head.info;
+		if (numberOfElements != 0) {
+			Node toDelete = head;
+			Object toDeleteInfo = head.info;
 
-		// the head becomes the former head's next node.
-		head = toDelete.next;
+			// the head becomes the former head's next node.
+			head = toDelete.next;
 
-		numberOfElements--;
-		return toDeleteInfo;
+			numberOfElements--;
+			return toDeleteInfo;
+		}
+
+		return null;
 	}
 
 	/**
 	 * Deletes the first node after an object.
+	 * 
 	 * @param ob
 	 * @return
 	 */
 	public Object deleteAfter(Object ob) {
 		current = head;
-		while(current.next != null) {
+		while (current.next != null) {
 			// node_a -> toDelete -> node_b
 			// node_a -> node_b
 			if (current.info == ob) {
 				// the node to delete must link the node
 				// right after itself.
 				// Therefore the next's next.
-				current.next = (current.next).next; 
+				current.next = (current.next).next;
 				numberOfElements--;
 				return current.next.info;
 			}
 
 			current = current.next;
 		}
+
 		return null;
 	}
 
 	/**
 	 * Assert wether or not the list is empty.
+	 * 
 	 * @return true or false depending the the state of the list.
 	 */
 	public boolean isEmpty() {
@@ -118,6 +128,7 @@ public class LinkedList
 
 	/**
 	 * Gives the lengh of the list
+	 * 
 	 * @return the lengh of the list.
 	 */
 	public int lengh() {
@@ -129,7 +140,7 @@ public class LinkedList
 	 */
 	public void printList() {
 		current = head;
-		while(current.next != null) {
+		while (current.next != null) {
 			System.out.print(current.info + " -> ");
 			current = current.next;
 		}
